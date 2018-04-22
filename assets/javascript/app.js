05476132bc846f26ecf94539d52bcc8e49698662
Ken <khuynh024@gmail.com>
2018-04-22 11:44:21 -0500

    working need to add gif still

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,14 +1,38 @@
+
+
+$(document).ready(function(){
+
+
+
 //gify api key
 var apiKey = "xJPC9qjcbQjUligbItqoskssH5Cd0KpZ";
 
+// dynamically create buttons
+var topics = ['Squirtle', 'Pikachu', 'Charizard'];
+
+function renderButtons(){
+    $('.btn-box').empty();
+    for (var i = 0; i < topics.length; i++){
+        var btn = $('<button>');
+        btn.addClass('btn btn-primary btnTopic');
+        btn.attr('data-name', topics[i]);
+        btn.text(topics[i]);
+
+        console.log('data-name', topics[i]);
+        $('.btn-box').append(btn);
+
+    }
+}
+
+//create initial buttons
+renderButtons();
 
 //function for ajax call
-$('.go').on('click', function(event){
+function displayGiphyButton(){
     //searcWord is the input from gify-input box
-    var searchWord = $('#gify-input').val();
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + searchWord + "&api_key=xJPC9qjcbQjUligbItqoskssH5Cd0KpZ&limit=5";
+    var btnData = $(this).attr('data-name');
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + btnData + "&api_key=xJPC9qjcbQjUligbItqoskssH5Cd0KpZ&limit=5";
 
-    console.log(searchWord);
     $('#gify-area').empty();
 
     $.ajax({
@@ -19,30 +43,40 @@ $('.go').on('click', function(event){
 
             var i = 0;
             for (i = 0; i < response.data.length; i++){
-                console.log(response.data);
-                //get rating
-                console.log(response.data[i].rating)
+                // console.log(response.data);
+                // console.log(response.data[i].rating)
                 
                 //get rating and append
-                var ratingDiv = $('<div>');
+                $('#gify-area').append('<br>' + '<br>');
+
+                var ratingDiv = $('<p>');
+                ratingDiv.attr('class', 'rating');
                 ratingDiv = response.data[i].rating;
                 $('#gify-area').append("Rating: " + ratingDiv);
     
                 //get image and append
                 var imageURL = response.data[i].images.downsized.url;
-                console.log(imageURL);
+                // console.log(imageURL);
                 var imageDiv = $('<img>');
-    
                 imageDiv.attr('src', imageURL);
-    
                 $('#gify-area').append(imageDiv);
             }
-
-
-
-
         })
+};
 
-    event.preventDefault();
+//function to add buttonTopics
+$('.go').on('click', function(e){
+    e.preventDefault();
+    var searchWord = $('#gify-input').val().trim();
+    topics.push(searchWord);
+
+    console.log(topics);
+    renderButtons();
+
 
 });
+
+$(document).on("click", ".btnTopic", displayGiphyButton);
+
+
+});//document.ready ending
